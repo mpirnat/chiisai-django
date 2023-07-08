5a0af33287dcb0c5d89a3f0dd0b19cc8f4b429c7
Mike Pirnat <mpirnat@gmail.com>
2023-07-08 17:20:38 -0400

    Add mass enable/disable of links and filtering by status to admin

--- chiisai/shortener/admin.py
+++ chiisai/shortener/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
 
-from .models import Link
+from .models import Link, Status
+
+
+@admin.action(description="Disable selected links")
+def disable_links(modeladmin, request, queryset):
+    queryset.update(status=Status.INACTIVE)
+
+
+@admin.action(description="Enable selected links")
+def enable_links(modeladmin, request, queryset):
+    queryset.update(status=Status.ACTIVE)
 
 
 class LinkAdmin(admin.ModelAdmin):
     list_display = ["alias", "url", "hits", "created", "updated", "status"]
+    list_filter = ["status"]
+
     fields = ["id", "created", "updated", "status", "alias", "url", "hits"]
     readonly_fields = ["id", "created", "updated"]
+
+    actions = [disable_links, enable_links]
 
 
 admin.site.register(Link, LinkAdmin)
