9ce273a17f1e3a17e41af6036b78e8563c13f6ea
Mike Pirnat <mpirnat@gmail.com>
2023-07-08 18:29:36 -0400

    Add a note about deliberate omissions from the API endpoints

--- chiisai/shortener/api.py
+++ chiisai/shortener/api.py
@@ -12,14 +12,19 @@ class LinkSchema(Schema):
     url: str
 
 
+@router.post("/v1/links")
+def create_short_url(request, data: LinkSchema):
+    link = Link(alias=data.alias, url=data.url)
+    link.save()
+    return str(link)
+
+
 @router.get("/v1/links/{alias}")
 def get_short_url_details(request, alias: str):
     link = Link.objects.get(alias=alias)
     return str(link)
 
 
-@router.post("/v1/links")
-def create_short_url(request, data: LinkSchema):
-    link = Link(alias=data.alias, url=data.url)
-    link.save()
-    return str(link)
+# Looking for an API for listing links from the collection,
+# or updating an existing link?
+# These have been omitted for security reasons.
