6081d73e28015234671c80b0f82fdd774acd5350
Mike Pirnat <mpirnat@gmail.com>
2023-07-08 16:53:11 -0400

    Create Link model, migration, and initial admin setup

--- chiisai/shortener/admin.py
+++ chiisai/shortener/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Link
+
+
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ["alias", "url", "hits", "created", "updated"]
+    fields = ["id", "created", "updated", "alias", "url", "hits"]
+    readonly_fields = ["id", "created", "updated"]
+
+
+admin.site.register(Link, LinkAdmin)
--- chiisai/shortener/models.py
+++ chiisai/shortener/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+    alias = models.SlugField(max_length=200, unique=True)
+    url = models.URLField(max_length=2048)
+    hits = models.PositiveBigIntegerField(default=0)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        get_latest_by = "created"
+        ordering = ["-created", "alias"]
+        indexes = [
+            models.Index(fields=["created"]),
+            models.Index(fields=["updated"]),
+            models.Index(fields=["url"]),
+            models.Index(fields=["hits"]),
+        ]
