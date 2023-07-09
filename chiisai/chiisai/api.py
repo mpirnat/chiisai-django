670fa898916132a0344214a7afe1366a1b7b5c6d
Mike Pirnat <mpirnat@gmail.com>
2023-07-09 17:14:30 -0400

    Require login to view API docs

--- chiisai/chiisai/api.py
+++ chiisai/chiisai/api.py
@@ -1,7 +1,8 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from ninja import NinjaAPI
 
 from shortener.api import router as shortener_router
 
-api = NinjaAPI()
+api = NinjaAPI(docs_decorator=staff_member_required)
 
 api.add_router("/shortener/", shortener_router)
