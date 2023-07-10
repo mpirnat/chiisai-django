cb3355b65565664e2c32e67b1e25502505ab5d16
Mike Pirnat <mpirnat@gmail.com>
2023-07-10 14:52:12 -0400

    Do the select for update in the get-or-404 to reduce queries

--- chiisai/shortener/views.py
+++ chiisai/shortener/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.db import transaction
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404
 
@@ -13,7 +14,15 @@ def redirect_alias_to_url(request, alias: str):
     """
     Look up and redirect to a long URL.
     """
-    link = get_object_or_404(Link, alias=alias, status=Status.ACTIVE)
+    link_queryset = Link.objects.filter(alias=alias, status=Status.ACTIVE)
+
     if settings.SHOULD_COUNT_HITS:
-        link.increment_hits()
+        link_queryset = link_queryset.select_for_update()
+        with transaction.atomic():
+            link = get_object_or_404(link_queryset)
+            link.hits += 1
+            link.save()
+    else:
+        link = get_object_or_404(link_queryset)
+
     return HttpResponseRedirect(link.url)
